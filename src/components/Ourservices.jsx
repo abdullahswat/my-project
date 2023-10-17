304eadd3b45be54e4188529a3652530703ebe3ac
abdullahswat <42508189+abdullahswat@users.noreply.github.com>
2023-10-17 13:11:16 +0500

    Update Ourservices.jsx
    Updated our services

--- src/components/Ourservices.jsx
+++ src/components/Ourservices.jsx
@@ -14,7 +14,7 @@ const Ourservices = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Our Services</p>
-              <p className='py-4'>// These are our services we offer</p>
+              <p className='py-4'>These are our services we offer</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
